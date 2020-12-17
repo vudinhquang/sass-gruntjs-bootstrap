cee81f7f9e2ad5ce8591ba694fa5e6c6153a2af4
vudinhquang <vudinhquangk53@gmail.com>
2020-12-18 02:02:03 +0900

    Cssmin 1

--- gruntjs/Gruntfile.js
+++ gruntjs/Gruntfile.js
@@ -1,18 +1,31 @@
 module.exports = function(grunt) {
     // 01 Config
     grunt.initConfig({
-        mPkg: grunt.file.readJSON('package.json')
+        mPkg: grunt.file.readJSON('package.json'),
+
+        // CSSMin
+        cssmin: {
+            options: {
+            },
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'development/css',
+                    src: ['case-01.css', 'case-02.css'],
+                    dest: 'production/css',
+                    ext: '.min.css'
+                }]
+            }
+        }
     });
 
     // 02 Load plugin
-    //grunt.loadNpmTasks('');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // 03 Register task
     grunt.registerTask('default', 'Log some stuff.', function() {
         grunt.log.write('Logging some stuff...').ok();
     });
 
-    grunt.registerTask('log', 'Log some stuff.', function() {
-        grunt.log.write('Logging some stuff 456...').ok();
-    });
+    grunt.registerTask('abc', ['cssmin']);
 };
