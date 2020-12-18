2e411a2628e83f3e48c58313d8e2d9e5e71180a3
vudinhquang <vudinhquangk53@gmail.com>
2020-12-18 11:03:14 +0900

    Concat 1

--- gruntjs/Gruntfile.js
+++ gruntjs/Gruntfile.js
@@ -2,6 +2,12 @@ module.exports = function(grunt) {
     // 01 Config
     grunt.initConfig({
         mPkg: grunt.file.readJSON('package.json'),
+		dirs: {
+			inputJS		: 'development/js',
+			inputCSS	: 'development/css',
+			outputJS	: 'production/js',
+			outputCSS	: 'production/css',
+		},
 
         // CSSMin
         cssmin: {
@@ -10,22 +16,37 @@ module.exports = function(grunt) {
             target: {
                 files: [{
                     expand: true,
-                    cwd: 'development/css',
+                    cwd: '<%= dirs.inputCSS %>',
                     src: ['case-01.css', 'case-02.css'],
-                    dest: 'production/css',
+                    dest: '<%= dirs.outputCSS %>',
                     ext: '.min.css'
                 }]
             }
-        }
+        },
+
+        // Concat
+        concat: {
+            options: {
+              separator: '\n',
+            },
+            dist: {
+				src: [
+					'<%= dirs.inputJS %>/case-01.js', 
+					'<%= dirs.inputJS %>/case-02.js', 
+				],
+				dest: '<%= dirs.outputJS %>/result.js',
+            },
+        },
     });
 
     // 02 Load plugin
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
     // 03 Register task
     grunt.registerTask('default', 'Log some stuff.', function() {
         grunt.log.write('Logging some stuff...').ok();
     });
 
-    grunt.registerTask('abc', ['cssmin']);
+    grunt.registerTask('abc', ['cssmin', 'concat']);
 };
---
+++ gruntjs/production/js/result.js
@@ -0,0 +1,2 @@
+console.log("FIle 01");
+console.log("FIle 02");
